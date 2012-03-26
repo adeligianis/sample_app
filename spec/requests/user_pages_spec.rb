1b81d2e2874d89e20f38b52bf1aa912c5d36d6ea
Adam Deligianis <adeligianis@gmail.com>
2012-03-26 16:07:40 -0400

    Completed Ch. 7 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,10 +20,19 @@ describe "User pages" do
   end
 
   describe "signup" do
+    
     before { visit signup_path }
+    
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button "Create my account" }.not_to change(User, :count)
+      end
+
+      describe "error messages" do
+        before { click_button "Create my account" }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -39,6 +48,14 @@ describe "User pages" do
         expect do
           click_button "Create my account"
         end.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Welcome") }
       end
     end
   end
