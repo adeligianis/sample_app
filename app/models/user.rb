14c76c2c997aedf00622c5bfda22bbce42db1cb7
Adam Deligianis <adeligianis@gmail.com>
2012-03-27 14:05:43 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@
 
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
+	has_secure_password
+	before_save :create_remember_token
 
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -20,4 +22,10 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 	has_secure_password
 	validates :password_confirmation, presence: true
+
+	private
+
+	  def create_remember_token
+	  	self.remember_token = SecureRandom.urlsafe_base64
+	  end
 end
